8156c5163c047aea38611ce6cbfdb1b09c1aa18d
Sergey Romadin <voidcode.music@gmail.com>
2021-03-02 15:40:59 +0300

    Доработал RestoService

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,21 @@
 export default class RestoService {
+    
+
+
     getMenuItems() {
-        return [];
+        const getResource = async(url) => {
+            const res = await fetch(url);
+            
+            if(!res.ok){
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+            }
+    
+            return await res.json();
+        };
+    
+        
+
+        return getResource("http://localhost:3004/menu")
+            .then((data) => data);
     }
 }
